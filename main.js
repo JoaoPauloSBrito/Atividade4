6ec1dc20637d735a699fce33f4cd725614c1aeb2
João Paulo Silva Brito <joaopaulobrito19012014@gmail.com>
2024-12-02 20:49:51 -0300

    Implementar função de cálculo do IMC com retorno do resultado.

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
+let altura;
+let peso;
+let resultado;
+
 function inicio(){
-    let altura = prompt("Digite sua altura: ");
-    let peso = prompt("Digite seu peso: ");
-    resultado = peso/(altura*altura);
-    alert(resultado);
+    resultado = calculo(); // Chama a função de cálculo
+    alert("Seu IMC é de: " + resultado); // Mostra o resultado com um alerta na tela
+}
+
+function calculo(){
+    // Repete a leitura da altura enquanto o usuário nao digitar um número válido
+    while (isNaN(altura)) {
+        altura = parseFloat(prompt("Digite a sua altura: "));
+    }
+    // Repete a leitura do peso enquanto o usuário nao digitar um número válido
+    while (isNaN(peso)) {
+        peso = parseFloat(prompt("Digite o seu peso: "));
+    } // Enquanto o peso não for um número, pede novamente
+    // Calcula o IMC
+    resultado = peso / (altura * altura);
+    return resultado;
 }
 inicio();
