52ff4a51455bb7f1844301283468b6ed3ebbcb7c
João Paulo Silva Brito <joaopaulobrito19012014@gmail.com>
2024-12-02 21:08:28 -0300

    Aplicando os retornos com as classificações de acordo com a faixa de IMC.

--- main.js
+++ main.js
@@ -3,21 +3,33 @@ let peso;
 let resultado;
 
 function inicio(){
-    resultado = calculo(); // Chama a função de cálculo
+    resultado = calculo();
     alert("Seu IMC é de: " + resultado); // Mostra o resultado com um alerta na tela
+    nivel = classificacao(resultado);
 }
 
 function calculo(){
-    // Repete a leitura da altura enquanto o usuário nao digitar um número válido
+    // Repete a leitura da altura enquanto o usuário nao digitar um número           válido
     while (isNaN(altura)) {
         altura = parseFloat(prompt("Digite a sua altura: "));
     }
-    // Repete a leitura do peso enquanto o usuário nao digitar um número válido
+    // Enquanto o peso não for um número, pede novamente  
     while (isNaN(peso)) {
         peso = parseFloat(prompt("Digite o seu peso: "));
-    } // Enquanto o peso não for um número, pede novamente
+    }
     // Calcula o IMC
     resultado = peso / (altura * altura);
     return resultado;
+}
+function classificacao(resultado){
+    if(resultado < 18.5){
+        alert("Você está abaixo do peso!");
+    } else if(resultado >= 18.5 && resultado < 24.9){
+        alert("Você está com o peso dentro do normal!");
+    } else if(resultado >= 25 && resultado < 29.9){
+        alert("Você está com sobrepeso!");
+    } else if(resultado >= 30){
+        alert("Você está obeso!");
+    }
 }
 inicio();
